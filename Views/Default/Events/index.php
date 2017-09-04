b7b9fa7db1eadb8be8412bbf1875bd40fb9934b4
sithembisoa <asithembiso@gmail.com>
2017-09-04 21:28:29 +0200

    Events Outline done

--- Views/Default/Events/index.php
+++ Views/Default/Events/index.php
@@ -1,47 +1,25 @@
 <?php $events = $template_data['events'] ?>
-<br>
-<br>
-<div class="table-responsive">
-	<table class="table">
-		<thead class="thead-inverse">
-			<tr>
-				<th style='text-transform: capitalize;'>id</th>
-				<th style='text-transform: capitalize;'>event name</th>
-				<th style='text-transform: capitalize;'>event title</th>
-				<th style='text-transform: capitalize;'>event description</th>
-				<th style='text-transform: capitalize;'>event date</th>
-				<th style='text-transform: capitalize;'>event location</th>
-				<th style='text-transform: capitalize;'>event tickets</th>
-				<th>Options</th>
-			</tr>
-		</thead>
-		<tbody>
 			<?php foreach ($events as $data  ): ?>
-				<tr>
-					<td data-limit-char="20"><?=$data["id"]?></td>
-					<td data-limit-char="20"><?=$data["event_name"]?></td>
-					<td data-limit-char="20"><?=$data["event_title"]?></td>
-					<td data-limit-char="20"><?=$data["event_description"]?></td>
-					<td data-limit-char="20"><?=$data["event_date"]?></td>
-					<td data-limit-char="20"><?=$data["event_location"]?></td>
-					<td data-limit-char="20"><?=$data["event_tickets"]?></td>
-
-					<td>
-						<a href="/events/edit/<?= $data['id'] ?>">
-							<button type="button" class="btn btn-default">Edite</button>
-						</a>
-						<a href="/events/delete/<?= $data['id'] ?>">
-							<button type="button" class="btn btn-default">Delete</button>
-						</a>
-						<a href="/events/view/<?= $data['id'] ?>">
-							<button type="button" class="btn btn-default">View</button>
-						</a>
-					</td>
-				</tr>
+					<div class="masonry-item col-md-4 col-sm-6 col-xs-12">
+			        <div class="post post-boxed">
+			            <div class="post-image">
+			                <img src="/Views/Default/Assets/News/<?= $data['image']== '0' ? 'no_image.png' : $data['image']?>" width="300px" alt="">
+			            </div>
+			            <ul class="post-meta">
+			                <li><span>Date: </span><?=$data["event_date"]?></li>
+			                <li><span>Event Name: </span><a href="#" class="text-capitalize"><?=$data["event_name"]?></a></li>
+			                <li><span>Location: </span><a href="#" class="text-capitalize"><?=$data["event_location"]?></a></li>
+											<li><span>Tickets: </span><a href="#" class="text-capitalize"><?=$data["event_tickets"]?></a></li>
+			            </ul>
+			            <div class="post-content">
+			                <h2 data-equalize-height="news"><?=$data["event_title"]?></h2>
+			                <p><?=berkaPhp\helpers\Str::limitChar($data["event_description"], 120, '...')?></p>
+			                <a href="/events/view/<?= $data['id'] ?>" class="btn btn-filled btn-sm btn-primary">View more</a>
+			            </div>
+			        </div>
+			    </div>
 			<?php endforeach ?>
-		</tbody>
-	</table>
-</div>
+
 <script>
 	$app.initList();
 </script>
