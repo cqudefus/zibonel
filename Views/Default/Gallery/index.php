43d3e8743ed6b10cf2a3a9b13f0ae3f6c040e083
sithembisoa <asithembiso@gmail.com>
2017-09-19 10:15:32 +0200

    fixed gallery navigation

--- Views/Default/Gallery/index.php
+++ Views/Default/Gallery/index.php
@@ -1,31 +1,31 @@
 <?php $gallery = $template_data['gallery'] ?>
-<div>
 <div class="container text-center">
 	<h3>Zibonele FM</h3>
     <p class="lead mb-60">Our Recent activities </p>
     <div class="masonry row">
-        <?php foreach ($gallery as $data  ): ?>
-            <div class="masonry-item webdesign col-md-4 col-sm-6 col-xs-12">
-                <!-- Image -->
-                <div class="image-box image-hover text-center">
-                    <div class="image">
-                        <a href="/Views/Default/Assets/Gallery/<?=$data["image_file"]?>" data-lightbox="<?=$data["image_heading"]?>" data-title="<?=$data["image_heading"]?>">
-                            <img src="/Views/Default/Assets/Gallery/<?=$data["image_file"]?>" alt="">
-                        </a>
-                    </div>
-                    <div class="title">
+        
+        <div>
+            <!-- Image --><div class="row">
+            <?php foreach ($gallery as $data  ): ?>
+                
+                     <div class="col-sm-3">
+            <div class="image-box image-hover text-center">
+                <div class="image">
+                    <a href="/Views/Default/Assets/Gallery/<?=$data["image_file"]?>" data-lightbox="gallery" data-title="Image Title"><img src="/Views/Default/Assets/Gallery/<?=$data["image_file"]?>" alt=""></a>
+                </div>
+                <div class="title">
+                    <a href="#">
                         <h5 class="mb-0"><?=$data["image_heading"]?></h5>
                         <span class="text-muted"><?=$data["image_description"]?></span>
-                        </a>
-                    </div>
+                    </a>
                 </div>
             </div>
-        <?php endforeach ?>
+        </div>
+            <?php endforeach ?></div>
+        </div>
     </div>
-
-
 </div>
-</div>
+
 <script>
 	$app.initList();
 </script>
