d6bfc0286d4328ace35ce37ba278edce5105fb4e
Leo Moko <mokoliyolo@gmail.com>
2017-09-21 13:56:06 -0700

    added lineup pictuers

--- Views/Default/Lineupschedule/index.php
+++ Views/Default/Lineupschedule/index.php
@@ -9,7 +9,7 @@
                 <div class="row">
                     <div class="col-md-4 col-sm-12 col-xs-12 ">
                         <div class="product-image">
-                            <img src="/Views/Default/Assets/Programs/podcasts.png" class="img-responsive">
+                            <img src="/Views/Default/Assets/Program/<?= $data['program_image'] ?>" class="img-responsive">
                         </div>
                     </div>
                     <div class="col-md-8 col-sm-12 col-xs-12">
